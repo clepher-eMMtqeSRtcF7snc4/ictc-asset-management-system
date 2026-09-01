e13994f2db6ba0d5a4ede7d12586db04d560a6dc
Clepher Jaudian <clepher.jaudian@msunaawan.edu.ph>
2026-09-02 05:09:06 +0800

    Navbar modified

--- apps/web/components/layout/navbar.tsx
+++ apps/web/components/layout/navbar.tsx
@@ -22,7 +22,7 @@ export function Navbar() {
     : []
 
   return (
-    <header className="relative z-20 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-background/95 px-4 backdrop-blur transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+    <header className="sticky top-0 relative z-20 flex h-16 shrink-0 items-center justify-between gap-2 border-b bg-sidebar px-4 backdrop-blur transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
       <div className="flex min-w-0 items-center gap-2">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mr-2 data-vertical:h-4 data-vertical:self-auto" />
